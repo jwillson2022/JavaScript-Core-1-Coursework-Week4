df6abc64f092d4e5b9d5f56bf2b2d2fb89489676
jonathan willson <jwillson2022@gmail.com>
2022-09-09 16:34:23 +0100

    J ex 1 & 2 work
    long way of doing things

--- 1-exercises/J-string-substring/exercise.js
+++ 1-exercises/J-string-substring/exercise.js
@@ -6,10 +6,15 @@
 
 let statement = "I like programming and dogs";
 
-statement = statement.substring();
+const split = statement.split();
+const slicer = statement.slice(1,7);
+const subStr = statement.substring(0,19);  //  and dogs (19,27);
+statement = subStr;
 
-console.log(statement);
-
+console.log(statement.length);  // 27 long
+console.log(slicer); //
+console.log(statement); //
+console.log(split); //
 /* EXPECTED OUTPUT 
 
   "I like programming"
